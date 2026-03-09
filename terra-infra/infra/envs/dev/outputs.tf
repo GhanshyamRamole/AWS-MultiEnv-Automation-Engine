ad49ee1b5bd85c57470475bf83b915a4d6de7e23
EC2 Default User <ec2-user@ip-172-31-23-228.us-west-1.compute.internal>
2026-03-09 12:01:39 +0000

    Infra-update

--- terra-infra/infra/envs/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ansible_controller_ip" {
-  description = "Public IP for the Dev Ansible Controller"
-  value       = module.dev_infrastructure.ansible_public_ip
-}
-
-output "dev_workload_ips" {
-  description = "Private IPs for Dev Docker/K8s nodes"
-  value       = module.dev_infrastructure.workload_ips
-}
